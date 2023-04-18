654a4f9e17344a92059fb8ffd687305ba2e4fd65
root <root@DESKTOP-OM63Q7N>
2023-04-17 23:01:54 -0300

    TDA cola implementado

--- src/cola.c
+++ src/cola.c
@@ -1,35 +1,46 @@
 #include "cola.h"
+#include "lista.h"
 
 cola_t *cola_crear()
 {
-	return NULL;
+	lista_t *lista = lista_crear();
+	return (cola_t *)lista;
 }
 
 cola_t *cola_encolar(cola_t *cola, void *elemento)
 {
-	return NULL;
+	if (!cola)
+		return NULL;
+	return (cola_t *)lista_insertar((lista_t *)cola, elemento);
 }
 
 void *cola_desencolar(cola_t *cola)
 {
-	return NULL;
+	if (!cola)
+		return NULL;
+	return lista_quitar_de_posicion((lista_t *)cola, 0);
 }
 
 void *cola_frente(cola_t *cola)
 {
-	return NULL;
+	if (!cola || cola_vacia(cola))
+		return NULL;
+	return lista_primero((lista_t *)cola);
 }
 
 size_t cola_tamanio(cola_t *cola)
 {
-	return 0;
+	if (!cola || cola_vacia(cola))
+		return 0;
+	return lista_tamanio((lista_t *)cola);
 }
 
 bool cola_vacia(cola_t *cola)
 {
-	return true;
+	return lista_vacia((lista_t *)cola);
 }
 
 void cola_destruir(cola_t *cola)
 {
+	lista_destruir((lista_t *)cola);
 }
